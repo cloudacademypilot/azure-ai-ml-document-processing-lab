76357ee74b2ee9dcaf48be41f6ba739eb9aa81bc
Aditya Hoode <v-ahoode@microsoft.com>
2023-01-02 18:24:14 +0530

    updated 01 validation.py

--- steps/01_using_form_recognizer_studio_for_document_processing/checks/01_used_form_recognizer_studio_for_document_processing/validation.py
+++ steps/01_using_form_recognizer_studio_for_document_processing/checks/01_used_form_recognizer_studio_for_document_processing/validation.py
@@ -1,9 +1,13 @@
 from azure.ai.formrecognizer import DocumentModelAdministrationClient 
 from azure.core.credentials import AzureKeyCredential 
 from azure.core.exceptions import ResourceNotFoundError
+import json
 
-endpoint = "https://fr-ahoode.cognitiveservices.azure.com/"
-credential = AzureKeyCredential("47edc0dca7904238a6eb1c04525717ca")
+with open("steps/01_using_form_recognizer_studio_for_document_processing/checks/01_used_form_recognizer_studio_for_document_processing/params.json") as f:
+    params = json.load(f)
+
+endpoint = params['endpoint']
+credential = AzureKeyCredential(params['key'])
 
 document_model_admin_client = DocumentModelAdministrationClient(endpoint, credential)
 account_details = document_model_admin_client.get_resource_details()
@@ -11,14 +15,18 @@ account_details = document_model_admin_client.get_resource_details()
 print("Our account has {} custom models, and we can have at most {} custom models".format(account_details.custom_document_models.count, account_details.custom_document_models.limit))
 
 # Here we get a paged list of all of our models
-models = document_model_admin_client.list_document_models()
-for model in models:
-    if model.model_id == "template-model":
-        print("Custom Template Model created")
-    else:
-        print("Custom Template Model not created")
+models = list(document_model_admin_client.list_document_models())
+model_ids = list()
 
-    if model.model_id == "neural-model":
-        print("Custom Neural Model created")
-    else:
-        print("Custom Neural Model not created")
+for model in models:
+    model_ids.append(model.model_id)
+
+if "template-model" in model_ids:
+    print("Custom Template Model created")
+else:
+    print("Custom Template Model not created")
+
+if "neural-model" in model_ids:
+    print("Custom Neural Model created")
+else:
+    print("Custom Neural Model not created")
