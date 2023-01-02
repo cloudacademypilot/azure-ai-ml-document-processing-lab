6f94cc29a6269d783e96c10c285f649ec66e0113
Aditya Hoode <v-ahoode@microsoft.com>
2023-01-02 21:14:33 +0530

    moved validation.py

--- steps/01_using_form_recognizer_studio_for_document_processing/checks/01_used_form_recognizer_studio_for_document_processing/validation.py
+++ steps/01_using_form_recognizer_studio_for_document_processing/checks/01_used_form_recognizer_studio_for_document_processing/validation.py
@@ -1,32 +1,31 @@
-from azure.ai.formrecognizer import DocumentModelAdministrationClient 
-from azure.core.credentials import AzureKeyCredential 
-from azure.core.exceptions import ResourceNotFoundError
+import os
+import sys
 import json
+import logging
+from azure.identity import AzureCliCredential
+from azure.mgmt.resource import ResourceManagementClient
+from azure.mgmt.subscription import SubscriptionClient
 
-with open("steps/01_using_form_recognizer_studio_for_document_processing/checks/01_used_form_recognizer_studio_for_document_processing/params.json") as f:
-    params = json.load(f)
+def handler():
+    try:
+        with open("steps/04_extract_data_using_custom_models/checks/04_extracted_data_using_custom_models/params.json") as f:
+            params = json.load(f)
+        subscription_id = params['subscription_id']
+        resource_group_name = params['resource_group_name']
+        credential = AzureCliCredential()
+        resource_client = ResourceManagementClient(credential, subscription_id)
+        resource_list = resource_client.resources.list_by_resource_group(resource_group_name, expand = "createdTime,changedTime")
+        resourcename = ''
+        for resource in resource_list:
+            if(resource.type == 'Microsoft.CognitiveServices/accounts'):
+                resourcename = resource.name
+                if(resourcename):
+                    print('Form Recognizer Studio is created')
+                else:
+                    print('Form Recognizer Studio is not created')
+    except Exception as e:
+        logging.error(e)
+        
+if __name__=="__main__":
+    handler()
 
-endpoint = params['endpoint']
-credential = AzureKeyCredential(params['key'])
-
-document_model_admin_client = DocumentModelAdministrationClient(endpoint, credential)
-account_details = document_model_admin_client.get_resource_details()
-
-print("Our account has {} custom models, and we can have at most {} custom models".format(account_details.custom_document_models.count, account_details.custom_document_models.limit))
-
-# Here we get a paged list of all of our models
-models = list(document_model_admin_client.list_document_models())
-model_ids = list()
-
-for model in models:
-    model_ids.append(model.model_id)
-
-if "template-model" in model_ids:
-    print("Custom Template Model created")
-else:
-    print("Custom Template Model not created")
-
-if "neural-model" in model_ids:
-    print("Custom Neural Model created")
-else:
-    print("Custom Neural Model not created")
