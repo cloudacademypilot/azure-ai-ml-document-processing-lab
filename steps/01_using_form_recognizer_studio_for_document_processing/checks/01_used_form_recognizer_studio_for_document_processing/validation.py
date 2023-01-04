ad31dc32ae31f29d7b9705101eafbefa47f0dc8f
Aditya Hoode <v-ahoode@microsoft.com>
2023-01-04 13:01:24 +0530

    modified validation scripts

--- steps/01_using_form_recognizer_studio_for_document_processing/checks/01_used_form_recognizer_studio_for_document_processing/validation.py
+++ steps/01_using_form_recognizer_studio_for_document_processing/checks/01_used_form_recognizer_studio_for_document_processing/validation.py
@@ -10,19 +10,21 @@ def handler():
     try:
         with open("steps/01_using_form_recognizer_studio_for_document_processing/checks/01_used_form_recognizer_studio_for_document_processing/params.json") as f:
             params = json.load(f)
+
         subscription_id = params['subscription_id']
         resource_group_name = params['resource_group_name']
+
         credential = AzureCliCredential()
+
         resource_client = ResourceManagementClient(credential, subscription_id)
         resource_list = resource_client.resources.list_by_resource_group(resource_group_name, expand = "createdTime,changedTime")
-        resourcename = ''
-        for resource in resource_list:
-            if(resource.type == 'Microsoft.CognitiveServices/accounts'):
-                resourcename = resource.name
-                if(resourcename):
-                    print('Form Recognizer Studio is created')
-                else:
-                    print('Form Recognizer Studio is not created')
+
+        fr_resource = [resource for resource in resource_list if resource.kind == "FormRecognizer"]
+        if(len(fr_resource) != 0):
+            print('Form Recognizer Studio is created')
+        else:
+            print('Form Recognizer Studio is not created')
+
     except Exception as e:
         logging.error(e)
         
--- steps/04_extract_data_using_custom_models/checks/04_extracted_data_using_custom_models/validation.py
+++ steps/04_extract_data_using_custom_models/checks/04_extracted_data_using_custom_models/validation.py
@@ -1,21 +1,36 @@
 from azure.ai.formrecognizer import DocumentModelAdministrationClient 
+from azure.identity import AzureCliCredential
 from azure.core.credentials import AzureKeyCredential 
 from azure.core.exceptions import ResourceNotFoundError
+from azure.mgmt.resource import ResourceManagementClient
 import json
+
 
 with open("steps/04_extract_data_using_custom_models/checks/04_extracted_data_using_custom_models/params.json") as f:
     params = json.load(f)
 
-endpoint = params['endpoint']
-credential = AzureKeyCredential(params['key'])
+subscription_id = params['subscription_id']
+resource_group_name = params['resource_group_name']
 
-document_model_admin_client = DocumentModelAdministrationClient(endpoint, credential)
+credential = AzureCliCredential()
+
+resource_client = ResourceManagementClient(credential, subscription_id)
+resource_list = resource_client.resources.list_by_resource_group(resource_group_name, expand = "createdTime,changedTime")
+
+for resource in resource_list:
+    if resource.kind == "FormRecognizer":
+        fr_res_name = resource.name
+
+endpoint = "https://" + fr_res_name + ".cognitiveservices.azure.com/"
+keyCredential = AzureKeyCredential(params['key'])
+
+document_model_admin_client = DocumentModelAdministrationClient(endpoint, keyCredential)
 account_details = document_model_admin_client.get_resource_details()
 
-print("Our account has {} custom models, and we can have at most {} custom models".format(account_details.custom_document_models.count, account_details.custom_document_models.limit))
+print("{} account has {} custom models, and we can have at most {} custom models".format(fr_res_name, account_details.custom_document_models.count, account_details.custom_document_models.limit))
 
 # Here we get a paged list of all of our models
-models = list(document_model_admin_client.list_document_models())
+models = document_model_admin_client.list_document_models()
 
 model_ids = [model.model_id for model in models if (model.model_id == "template-model") or (model.model_id == "neural-model")]
 
