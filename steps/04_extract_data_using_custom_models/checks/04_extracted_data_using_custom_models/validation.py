a34136991982b90c7381f9640a8ce227757f0e3a
Aditya Hoode <v-ahoode@microsoft.com>
2023-01-05 14:45:32 +0530

    updated 04 validation.py

--- steps/04_extract_data_using_custom_models/checks/04_extracted_data_using_custom_models/validation.py
+++ steps/04_extract_data_using_custom_models/checks/04_extracted_data_using_custom_models/validation.py
@@ -1,9 +1,10 @@
 from azure.ai.formrecognizer import DocumentModelAdministrationClient 
 from azure.identity import AzureCliCredential
-from azure.core.credentials import AzureKeyCredential 
+from azure.core.credentials import AzureKeyCredential
 from azure.core.exceptions import ResourceNotFoundError
 from azure.mgmt.resource import ResourceManagementClient
 import json
+import requests
 
 
 with open("steps/04_extract_data_using_custom_models/checks/04_extracted_data_using_custom_models/params.json") as f:
@@ -13,6 +14,7 @@ subscription_id = params['subscription_id']
 resource_group_name = params['resource_group_name']
 
 credential = AzureCliCredential()
+token = credential.get_token("https://management.azure.com").token
 
 resource_client = ResourceManagementClient(credential, subscription_id)
 resource_list = resource_client.resources.list_by_resource_group(resource_group_name, expand = "createdTime,changedTime")
@@ -22,7 +24,18 @@ for resource in resource_list:
         fr_res_name = resource.name
 
 endpoint = "https://" + fr_res_name + ".cognitiveservices.azure.com/"
-keyCredential = AzureKeyCredential(params['key'])
+
+listKeysUrl = "https://management.azure.com/subscriptions/" + subscription_id + "/resourceGroups/" + resource_group_name + "/providers/Microsoft.CognitiveServices/accounts/" + fr_res_name + "/listKeys?api-version=2021-10-01"
+headers = {
+    'ContentType': 'application/json',
+    'Authorization': f'Bearer {token}'
+}
+
+response = requests.post(url=listKeysUrl, headers=headers)
+if response.status_code == 200:
+    key1 = response.json()['key1']
+
+keyCredential = AzureKeyCredential(key1)
 
 document_model_admin_client = DocumentModelAdministrationClient(endpoint, keyCredential)
 account_details = document_model_admin_client.get_resource_details()
